d9a5fb00383d0c47386b43b8326c6d11a26bdf8a
silversoren <ehsoren9@gmail.com>
2022-03-16 23:55:14 +0900

    test

--- SinchonBeer-dev/SinchonBeer-Build/src/main/java/com/bitcamp/sc/pay/service/impl/type/KakaoPay.java
+++ SinchonBeer-dev/SinchonBeer-Build/src/main/java/com/bitcamp/sc/pay/service/impl/type/KakaoPay.java
@@ -20,8 +20,9 @@ import com.bitcamp.sc.pay.domain.KakaoPayReady;
 @Service
 public class KakaoPay {
 
-//	private static final String HOST = "http://localhost:8081/";
-	private static final String HOST = "http://3.35.0.242:8081/";
+//	private static final String HOST = "http://localhost:8080/";
+//	private static final String HOST = "http://3.35.0.242:8081/";
+	private static final String HOST = "http://15.164.250.25:8080/";
 	
 	private KakaoPayReady kakaoPayReady;
 	private KakaoPayApproval kakaoPayApproval;
